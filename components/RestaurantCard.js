dd3ee23da82990f24d6f649e5f7c9b06e92cb8c9
Adarsha-React <adarshareact2023@gmail.com>
2023-07-06 16:08:29 +0530

    Restaurant Card and Styled with CSS

--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -7,11 +7,14 @@ const RestaurantCard = () => {
     <div className="restaurant-card" style={styleCard}>
       <img
         className="restaurant-image"
+        alt="resImage"
         src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/e33e1d3ba7d6b2bb0d45e1001b731fcf"
       />
-      <h4>Burger King</h4>
-      <h5>Burgers, American</h5>
-      <h6>4.2 stars</h6>
+      <div className="card-details">
+        <h4>Burger King</h4>
+        <h6>Burgers, American</h6>
+        <h6>4.2 stars</h6>
+      </div>
     </div>
   );
 };
