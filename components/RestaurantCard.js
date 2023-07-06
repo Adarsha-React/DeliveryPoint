0d12d1af4c1023980331480f2f2b25b8676238bf
Adarsha-React <adarshareact2023@gmail.com>
2023-07-06 16:32:05 +0530

    Get the data from the Object into restaurantCard

--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -3,17 +3,25 @@ const RestaurantCard = () => {
     backgroundColor: "#f0f0f0",
   };
 
+  const restaurant = {
+    imageUrl:
+      "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/e33e1d3ba7d6b2bb0d45e1001b731fcf",
+    name: "Burger King",
+    cuisines: "Burgers, American",
+    avgRataing: "4.2",
+  };
+
   return (
-    <div className="restaurant-card" style={styleCard}>
+    <div className="restaurant-card">
       <img
         className="restaurant-image"
         alt="resImage"
-        src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_264,h_288,c_fill/e33e1d3ba7d6b2bb0d45e1001b731fcf"
+        src={restaurant.imageUrl}
       />
       <div className="card-details">
-        <h4>Burger King</h4>
-        <h6>Burgers, American</h6>
-        <h6>4.2 stars</h6>
+        <h4>{restaurant.name}</h4>
+        <h6>{restaurant.cuisines}</h6>
+        <h6>{restaurant.avgRataing} stars</h6>
       </div>
     </div>
   );
