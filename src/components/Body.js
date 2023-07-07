ad97564d5fe585c65ea34628bb2ed2e0be4f7de9
Adarsha-React <adarshareact2023@gmail.com>
2023-07-07 12:39:53 +0530

    Added sorting and filterring the restaurants

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,17 +1,44 @@
 import RestaurantCard from "./RestaurantCard";
 import { restaurantList } from "../constants";
+import { useState } from "react";
 
 const Body = () => {
-  console.log(restaurantList);
+  const [listOfRestaurants, setListOfRestaurants] = useState(restaurantList);
 
+  console.log(<Body />);
   return (
     <div className="body">
-      <div className="search-container">
-        <input type="text" placeholder="Search" />
-        <button className="search-btn">Search</button>
+      <div className="filter-container">
+        <div>
+          <input type="text" placeholder="Search" />
+          <button className="search-btn">Search</button>
+        </div>
+        <div>
+          <button
+            className="filter-btn"
+            onClick={() => {
+              const sortedRestaurants = [...restaurantList].sort(
+                (a, b) => a.data.avgRating - b.data.avgRating
+              );
+              setListOfRestaurants(sortedRestaurants);
+            }}
+          >
+            Ratings
+          </button>
+          <button
+            onClick={() => {
+              const filterRestaurant = restaurantList.filter(
+                (restaurant) => restaurant?.data?.avgRating > 4.2
+              );
+              setListOfRestaurants(filterRestaurant);
+            }}
+          >
+            Top Rated
+          </button>
+        </div>
       </div>
       <div className="restaurant-container">
-        {restaurantList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard
             restaurant={restaurant?.data}
             key={restaurant?.data?.id}
